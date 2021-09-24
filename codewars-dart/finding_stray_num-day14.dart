71ae31afe712890e1b56800a51550c06c656ad5a
S-Sahanii0 <shakyasahani0@gmail.com>
2021-09-24 20:51:39 +0545

    finding stray

---
+++ codewars-dart/finding_stray_num-day14.dart
@@ -0,0 +1,16 @@
+stray(List<int> numbers) {
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] != numbers[i + 1]) {
+      if (i == 0) {
+        return numbers[i];
+      } else {
+        return numbers[i + 1];
+      }
+    }
+  }
+}
+
+void main() {
+  print(stray([1, 1, 2]));
+  stray([1, 1, 2]);
+}
