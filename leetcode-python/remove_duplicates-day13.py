3067ae3127a3c8762cf72cb97cd160b5e3a9908d
S-Sahanii0 <shakyasahani0@gmail.com>
2021-09-24 10:45:04 +0545

    minor change

--- leetcode-python/remove_duplicates-day13.py
+++ leetcode-python/remove_duplicates-day13.py
@@ -4,6 +4,23 @@ from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
+        i = 0
+        while i < (len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                del nums[i]
+            else:
+                i += 1
+        print(nums)
+        print(len(nums))
+        return len(nums)
+
+   
+
+Solution().removeDuplicates(nums=[1,1,2])
+
+#Another Solution:
+def removeDuplicates(self, nums: List[int]) -> int:
+
         length = 0
         if len(nums) == 0: return length
         for i in range(1,len(nums)):
@@ -11,5 +28,4 @@ class Solution:
                 length += 1
                 nums[length] = nums[i]
         return length+1
-Solution().removeDuplicates(nums=[1,2,2,3,3])
         
