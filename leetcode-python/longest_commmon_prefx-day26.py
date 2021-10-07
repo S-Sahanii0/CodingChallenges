c25f217ec9ff99ea81975ccb62aca9065a4e9148
S-Sahanii0 <shakyasahani0@gmail.com>
2021-10-07 22:16:39 +0545

    day 26

---
+++ leetcode-python/longest_commmon_prefx-day26.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        s = ''
+        k = 0
+        while k < len(strs[0]):
+            c = strs[0][k]
+            for i in range(1, len(strs)):
+                if k >= len(strs[i]) or c != strs[i][k]:
+                    return s
+            s += c
+            k += 1
+        return s
