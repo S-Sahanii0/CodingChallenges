73b4fd7642cf44aec81f9d2d593bc041bccc500d
S-Sahanii0 <shakyasahani0@gmail.com>
2021-10-27 14:46:21 +0545

    day 36

---
+++ leetcode-python/find_cycle-day36.py
@@ -0,0 +1,13 @@
+
+from typing import Optional
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
