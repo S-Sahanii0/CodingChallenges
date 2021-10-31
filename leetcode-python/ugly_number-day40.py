92ca28c942f82f01295441fde61ef741dbe05977
S-Sahanii0 <shakyasahani0@gmail.com>
2021-10-31 15:18:38 +0545

    day 40

--- leetcode-python/ugly_number-day40.py
+++ leetcode-python/ugly_number-day40.py
@@ -4,7 +4,7 @@ class Solution:
 
     def isUgly(self, n: int) -> bool:
         while n % 2 == 0 and n > 1:
-        n //= 2
+            n //= 2
         
         while n % 3 == 0 and n > 1:
             n //= 3
@@ -12,6 +12,6 @@ class Solution:
         while n % 5 == 0 and n > 1:
             n //= 5
             
-    return n == 1
+        return n == 1
 
-Solution().isUgly(n=14)
+
