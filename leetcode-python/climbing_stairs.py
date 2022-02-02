f4a087ab3ff2a05d63897a6c56787d14020f2e64
S-Sahanii0 <shakyasahani0@gmail.com>
2022-02-02 22:00:21 +0545

    completed: climbing stairs

--- leetcode-python/climbing_stairs.py
+++ leetcode-python/climbing_stairs.py
@@ -3,34 +3,16 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        onesAndtwos = list(divmod(n , 2))
-        if onesAndtwos[1] != 0:
-            countOfSteps = [1]
-        else:
-            countOfSteps = []
-        
-        if onesAndtwos[0] % 2 == 0:
-                while onesAndtwos[0] != 0:
-                    countOfSteps.append(2)
-                    onesAndtwos[0] = onesAndtwos[0] - 2
-        else:
-                while onesAndtwos[0] != 1:
-                    countOfSteps.append(2)
-                    onesAndtwos[0] = onesAndtwos[0] - 2
-                countOfSteps.append(1)
-        print(countOfSteps)
-       
-        onlyTwo = list(filter(lambda x: x % 2 == 0, countOfSteps))
-       
-        result = len(countOfSteps)
-        for i in range(1, len(onlyTwo)-1):
-            result = result + 2
-            
-            
-        print(result)
-        return result
+        # easy to understand: bottom-up solution
+        if n < 3:
+            return n
+        x, y = 1, 2
+        for i in range(n - 2):
+            x, y = y, x + y
+        return y
+
    
-Solution().climbStairs(n=5)
+Solution().climbStairs(n=1)
         
     
        
