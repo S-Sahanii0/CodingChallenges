317d0249339f8e02ee8dcda0b2e1985c0b1a7c64
S-Sahanii0 <shakyasahani0@gmail.com>
2022-05-25 21:54:32 +0545

    day 4: palindrome

---
+++ leetcode-python/palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reverse = 0 
+        temp = x
+        while temp > 0:
+            remainder = temp % 10 
+            reverse = reverse * 10 + remainder
+            temp = temp // 10
+        return reverse == x
+            
